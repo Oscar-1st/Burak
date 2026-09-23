dc2a9e3d4ae0d97097145e98de13003a5eaf9e7c
Oscar-1st <olimjonovogabekotabek@gmail.com>
2026-09-24 02:36:58 +0900

    feat: TASK-M completed

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,8 @@ app.use(express.json());
 /** 2-SESSIONS **/
 
 /** 3-VIEWS **/
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 /** 4-ROUTERS **/
 
--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,15 @@
+function getSquareNumbers(arr: number[]){
+    return arr.map(num => ({ number: num, square: num * num }));
+};
+let result = getSquareNumbers([1, 2, 3])
+console.log(result);
+
+// TASK M: 4
+
+// Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni kvadratidan tashkil topgan object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin.
+// MASALAN: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}];
+
+
 //  function reverseSentence(str) {
 // let words = str.split(" ");
 // for(let i = 0; i < words.length; i++){
