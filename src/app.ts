ecaa56fc6b5c6c6c1eb5b10b492a1fac1f67c82b
Oscar-1st <olimjonovogabekotabek@gmail.com>
2026-09-24 01:44:53 +0900

    feat: express integrations

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+
+/** 1-ENTRANCE **/ 
+const app = express();
+console.log("dirname:", __dirname);
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+/** 2-SESSIONS **/
+
+/** 3-VIEWS **/
+
+/** 4-ROUTERS **/
+
+export default app;  // module.exports = app;  // commonjs
